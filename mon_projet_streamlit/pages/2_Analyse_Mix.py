f85f7375bc358e320f79004fb83bd92c1b222b53
Chupl1 <pierrechup1@gmail.com>
2026-06-23 11:44:30 +0200

    Update 2_Analyse_Mix.py

--- mon_projet_streamlit/pages/2_Analyse_Mix.py
+++ mon_projet_streamlit/pages/2_Analyse_Mix.py
@@ -4,7 +4,7 @@ import plotly.express as px
 
 st.set_page_config(page_title="Eco2mix Dashboard - Analyse Mix", page_icon="📈", layout="wide")
 
-df = pd.read_csv('eco2mix_regional_journalier.csv', sep=';', decimal='.', encoding='utf-8-sig')
+df = pd.read_csv('mon_projet_streamlit/eco2mix_regional_journalier.csv', sep=';', decimal='.', encoding='utf-8-sig')
 df['Date'] = pd.to_datetime(df['Date'])
 df['Annee'] = df['Date'].dt.year
 df['Mois'] = df['Date'].dt.month
